d87b85815d046439ed862cac3a8a859900a3bf30
Jojo3235 <joralmand@gmail.com>
2022-12-19 09:09:44 +0100

    ejercicio5 completo

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,4 +1,25 @@
+import doctest
 
+def pedir_numero():
+    while True:
+        try:
+            numero = int(input("Introduce un número: "))
+            if numero < 0:
+                print("El número debe ser positivo")
+            else:
+                return numero
+        except ValueError:
+            print("El valor introducido no es un número")
 
-def hollow_triangle(altura):
-    pass
+def hollow_triangle():
+    altura = pedir_numero()
+    for i in range(altura):
+        print('_'*(altura-i)+'#'*(1+2*i)+'_'*(altura-i))
+
+def main():
+    hollow_triangle()
+
+if __name__ == '__main__':
+    main()
+    doctest.testmod()
+
