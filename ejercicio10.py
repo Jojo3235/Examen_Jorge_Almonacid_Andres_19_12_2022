659903ba7a01a564c41c6725339fdb359a38328e
Jojo3235 <joralmand@gmail.com>
2022-12-19 10:51:17 +0100

    ejercicio 10 completo

--- ejercicio10.py
+++ ejercicio10.py
@@ -1,3 +1,7 @@
+#importamos el módulo doctest
+import doctest
+
+#Creamos la función pedir número
 def pedir_numero():
     while True:
         try:
@@ -9,8 +13,38 @@ def pedir_numero():
         except ValueError:
             print("El valor introducido no es un número")
 
-def crear_lista_n_numeros():
-    pass
+#Creamos fibonacci de n terminos
+def xbonacci(lista, n):
+    resultado = []
+    for i in range(len(lista)):
+        resultado.append(lista[i])
+    for i in range(len(lista), n):
+        siguiente_elemento = sum(resultado[-len(lista):])
+        resultado.append(siguiente_elemento)
+    return resultado
 
-def nbonacci(n):
-    pass
+#Creamos una función para crear una lista que vaya añadiendo numeros hasta que se pare
+def crear_lista():
+    lista = []
+    while True:
+        try:
+            numero = int(input("Introduce un número: "))
+            if numero < 0:
+                print("El número debe ser positivo")
+            else:
+                lista.append(numero)
+        except ValueError:
+            print("El valor introducido no es un número")
+            break
+    return lista
+
+#Creamos la función main
+def main():
+    lista = crear_lista()
+    n = pedir_numero()
+    print(xbonacci(lista, n))
+
+#Llamamos a la función main
+if __name__ == '__main__':
+    main()
+    doctest.testmod(main())
