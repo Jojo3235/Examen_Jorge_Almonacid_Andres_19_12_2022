2857d0734c4d511f058271bc7b3f3429f4082260
Jojo3235 <joralmand@gmail.com>
2022-12-19 09:01:50 +0100

    commit inicial ejercicios

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class Nonograma:
+    def __init__(self, clues):
+        pass
+
+    def solve(self):
+        pass
---
+++ ejercicio3.py
@@ -0,0 +1,25 @@
+numeros = {0: 'zero',
+            1: 'one',
+            2: 'two',
+            3: 'three',
+            4: 'four',
+            5: 'five',
+            6: 'six',
+            7: 'seven',
+            8: 'eight',
+            9: 'nine'
+            }
+
+def pedir_numero():
+    while True:
+        try:
+            numero = int(input("Introduce un número: "))
+            if numero < 0:
+                print("El número debe ser positivo")
+            else:
+                return numero
+        except ValueError:
+            print("El valor introducido no es un número")
+
+def numbersOfLetters(n):
+    pass
