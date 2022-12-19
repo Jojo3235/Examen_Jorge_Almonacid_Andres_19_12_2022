917f6b0843dad30642e9bc6466760131a710e30e
Jojo3235 <joralmand@gmail.com>
2022-12-19 09:28:52 +0100

    ejercicio 8

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,2 +1,16 @@
+import doctest 
+
 def three_fifty(oracion):
-    pass
+    oracion = oracion.lower()
+    if 'three fifty' in oracion or '3.50' in oracion or 'tree fiddy' in oracion:
+        return 'Es el monstruo del Lago Ness'
+    else:
+        return 'No es el monstruo del Lago Ness'
+
+def main():
+    oracion = input('Introduce una oración: ')
+    print(three_fifty(oracion))
+
+if __name__ == '__main__':
+    main()
+    doctest.testmod()
