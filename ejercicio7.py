0dbb6a081d897dc17b027ae141658b8581e9747d
Jojo3235 <joralmand@gmail.com>
2022-12-19 09:24:17 +0100

    ejercicio 7 tablero

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,8 +1,11 @@
-import numpy
+import numpy as np
+
+tablero = np.full((8,8), ' ')
 
 class Caballo:
     def __init__(self, posicion):
         self.posicion = posicion
-
+    
     def movimiento():
         pass
+
