619b6dbd7e1e0228b2a423a2557bc0a32cbb5bf0
Jojo3235 <joralmand@gmail.com>
2022-12-19 11:06:33 +0100

    ej 7 fin

--- ejercicio4.py
+++ ejercicio4.py
@@ -11,4 +11,3 @@ def sort_letras(numeros):
 def do_math(numero):
     pass
 
-print(separar_numeros('1 2 3 4 5 6 7 8 9 10'))
--- ejercicio7.py
+++ ejercicio7.py
@@ -1,11 +1,47 @@
+#Importamos las librerias
 import numpy as np
+import doctest
 
-tablero = np.full((8,8), ' ')
-
+#Creamos la clase Caballo con la funcion movimiento como metodo
 class Caballo:
     def __init__(self, posicion):
-        self.posicion = posicion
+        self.fila = posicion[0]
+        self.columna = posicion[1]
     
-    def movimiento():
-        pass
+    def movimiento(self):
+        distancia =  np.abs(np.subtract.outer(np.arange(8),np.arange(8)))
+        return distancia[self.fila][self.columna]
 
+#Creamos la funcion para pedir un numero
+def pedir_numero():
+    while True:
+        try:
+            numero = int(input("Introduce un número: "))
+            if numero < 0 and numero > 8:
+                print("El número debe ser positivo")
+            else:
+                return numero
+        except ValueError:
+            print("El valor introducido no es un número")
+
+#Creamos la funcion para pedir una lista
+def lista_posicion():
+    lista = []
+    posicion1 = pedir_numero()
+    posicion2 = pedir_numero()
+    lista.append(posicion1)
+    lista.append(posicion2)
+    return lista
+
+#Creamos la función main
+def main():
+    tablero = np.full((8,8), ' ')
+    posicion = lista_posicion()
+    caballo = Caballo(posicion)
+    posicion_deseadad = lista_posicion()
+    print(caballo.movimiento())
+
+#Llamamos a la función main
+if __name__ == '__main__':
+    main()
+    doctest.testmod(main())
